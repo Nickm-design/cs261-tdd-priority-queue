701170b3117cae6616cb909489a4884541254066
Nick <morrinic@oregonstate.edu>
2020-11-22 21:08:27 -0800

    Test enqueue efficiency works

--- test_naive_priority_queue.py
+++ test_naive_priority_queue.py
@@ -208,17 +208,17 @@ class TestNaivePriorityQueue(unittest.TestCase):
             time_samples.append(end_time - start_time)
         small_average_enqueue_time = sum(time_samples) / float(len(time_samples))
 
-    #     large_queue = NaivePriorityQueue()
-    #     for _ in range(0, 1000000):
-    #         large_queue.enqueue('fake')
-    #     large_time_samples = []
-    #     for _ in range(0, 1000):
-    #         start_time = time.time()
-    #         large_queue.enqueue('fake')
-    #         end_time = time.time()
-    #         large_time_samples.append(end_time - start_time)
-    #     large_average_enqueue_time = sum(large_time_samples) / float(len(large_time_samples))
-    #     self.assertAlmostEqual(small_average_enqueue_time, large_average_enqueue_time, delta=small_average_enqueue_time)
+        large_queue = NaivePriorityQueue()
+        for _ in range(0, 1000000):
+            large_queue.enqueue('fake')
+        large_time_samples = []
+        for _ in range(0, 1000):
+            start_time = time.time()
+            large_queue.enqueue('fake')
+            end_time = time.time()
+            large_time_samples.append(end_time - start_time)
+        large_average_enqueue_time = sum(large_time_samples) / float(len(large_time_samples))
+        self.assertAlmostEqual(small_average_enqueue_time, large_average_enqueue_time, delta=small_average_enqueue_time)
 
     # While enqueing naively is efficient... what is the complexity of dequeuing?
 
