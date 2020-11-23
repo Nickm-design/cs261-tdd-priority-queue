575f909d4ee41acc99b059260fe6d489a6f76a5c
Nick <morrinic@oregonstate.edu>
2020-11-22 21:19:03 -0800

    Test instantiation works

--- max_heap.py
+++ max_heap.py
@@ -3,8 +3,9 @@
 # If you can't figure it out recursively, use a loop. (But then refactor
 # your implementation into a recursive one!)
 # Your implementation should pass the tests in test_max_heap.py.
-# YOUR NAME
+# Nick Morris
 
 class MaxHeap:
 
-   pass
+   def __init__(self):
+      pass
